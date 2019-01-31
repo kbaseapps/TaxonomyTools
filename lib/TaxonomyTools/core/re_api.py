c17e57ae9ed6cdafd2b79266110d141c634702d5
Sean R. McCorkle <sean.r.mccorkle@gmail.com>
2019-01-31 13:16:45 -0500

    [WIP] added RE api call

--- lib/TaxonomyTools/core/re_api.py
+++ lib/TaxonomyTools/core/re_api.py
@@ -16,14 +16,32 @@ class RE_API:
         ret = requests.post(self.re_url+endpoint, data, params=params, headers=header)
         return ret.json()
 
-    def get_related_sequences(self, rid, sf_sim=1, df_sim=1, exclude_self=0):
-        if not rid.startswith('rxn_reaction/'):
-            rid = 'rxn_reaction/' + rid
-        body = json.dumps({'rid': rid, 'sf_sim': sf_sim, 'df_sim': df_sim,
-                           'exclude_self': exclude_self})
-        ret = self._call_re(params={'view': "list_genes_for_similar_reactions"}, data=body)
+
+    def get_referred_counts_by_type(self, ws_ref, is_private=False, is_public=False, owners=[],
+                                         simplify_type=True ):
+
+        if not isinstance( owners, list ):
+            raise RuntimeError( f"parameter 'owners' not a list: {owners}" )
+        # maybe ensure each element of owners is type 'str'?
+
+        if len( owners ) < 1:
+            owners = False
+        
+        body = json.dumps({'key':            ws_ref.replace( '/', ':'),
+                           'is_private':     is_private,
+                           'is_public':      is_public,
+                           'owners':         owners,
+                           'simplify_type':  simplify_type
+                          })
+        logging.info( "############## json body {0}".format( pformat( body ) ) )
+        ret = self._call_re(params={'view': "list_referencing_type_counts"}, data=body)
+        #ret = []
         if "error" in ret:
             raise RuntimeError(f"{ret['error']}: {ret.get('arango_message', '')}")
-        logging.info(f"Found {ret['results'][0]['count']} related sequences")
-        return ret['results'][0]['sequences']
+        #logging.info(f"Found {ret['results'][0]['count']} related sequences")
+        logging.info( "########### Arango query return {0}".format( pformat( ret ) ) )
+
+        return ret
+
+        #return ret['results'][0]['sequences']
 
